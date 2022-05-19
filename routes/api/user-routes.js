c694fb9cf0e3761bcb13a6bce7ff2f1fc0ed857b
brettshackett <63305041+brettshackett@users.noreply.github.com>
2022-05-18 22:44:32 -0400

    added js files to create routes for the api  as well as create the schema file in order to create the users table, used insomnia to update the tables

---
+++ routes/api/user-routes.js
@@ -0,0 +1,101 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET /api/users
+//selects all users from the user table in the database and send it back as JSON
+router.get('/', (req, res) => {
+    // Access our User model and run .findAll() method)
+    User.findAll({
+        attributes: { exclude: ['password'] }
+      })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+  });
+
+// GET /api/users/1
+//we use the where option to indicate we want to find a user where its id value equals whatever req.params.id is
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+          id: req.params.id
+        }
+      })
+        .then(dbUserData => {
+          if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+  });
+
+// POST /api/users
+//to insert data we pass in key/value pairs where the keys are what we defined in teh user model and the values are what we get from req.body
+router.post('/', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+// PUT /api/users/1
+//this update method combines the parameters for creating data and looking up data. we pass in req.body to provide the new data we want to use in the update and req.params.id to indicate where exactly we want that new data to be used. 
+router.put('/:id', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  
+    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+    User.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData[0]) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+// DELETE /api/users/1
+//to delete data, we use the destroy method and provide some type of identifier to indicate where exactly we would like to delete data from the user database table. 
+router.delete('/:id', (req, res) => {
+    User.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
