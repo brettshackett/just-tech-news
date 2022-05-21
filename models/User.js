2933df4cb611f14295b1cd6b181ce9b2b2734caf
brettshackett <63305041+brettshackett@users.noreply.github.com>
2022-05-20 21:07:17 -0400

    updated routes and added password hashing to boost security

--- models/User.js
+++ models/User.js
@@ -1,9 +1,14 @@
 //imported model class from datatypes object; this model class is what we create our own models from using the extends keyword so user inherits all of the fucntionality the model class has.
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 //create the user model
-class User extends Model{}
+class User extends Model{
+  checkPassword(loginPw){
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 //define table columns and configuration
 //once we create the user class, we use the init method to initialize the model's data and configuration, passing in two objects as arguments.
@@ -11,44 +16,45 @@ class User extends Model{}
 //the second object configures certain options for the table
 User.init(
     {
-      // define an id column
       id: {
-        // use the special Sequelize DataTypes object provide what type of data it is
         type: DataTypes.INTEGER,
-        // this is the equivalent of SQL's `NOT NULL` option
         allowNull: false,
-        // instruct that this is the Primary Key
         primaryKey: true,
-        // turn on auto increment
         autoIncrement: true
       },
-      // define a username column
       username: {
         type: DataTypes.STRING,
         allowNull: false
       },
-      // define an email column
       email: {
         type: DataTypes.STRING,
         allowNull: false,
-        // there cannot be any duplicate email values in this table
         unique: true,
-        // if allowNull is set to false, we can run our data through validators before creating the table data
         validate: {
           isEmail: true
         }
       },
-      // define a password column
       password: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          // this means the password must be at least four characters long
           len: [4]
         }
       }
     },
     {
+      hooks: {
+        // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+        // set up beforeUpdate lifecycle "hook" functionality
+        async beforeUpdate(updatedUserData) {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+        }
+      },
       sequelize,
       timestamps: false,
       freezeTableName: true,
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -53,6 +53,28 @@ router.post('/', (req, res) => {
       });
   });
 
+  router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  });
+
 // PUT /api/users/1
 //this update method combines the parameters for creating data and looking up data. we pass in req.body to provide the new data we want to use in the update and req.params.id to indicate where exactly we want that new data to be used. 
 router.put('/:id', (req, res) => {
@@ -60,6 +82,7 @@ router.put('/:id', (req, res) => {
   
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
     User.update(req.body, {
+      individualHooks: true,
       where: {
         id: req.params.id
       }
